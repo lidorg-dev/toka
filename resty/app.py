6be6ba359ac563420d7f8d59fd32b84817641cf5
Ubuntu <ubuntu@ip-172-31-80-143.ec2.internal>
2023-03-04 16:29:13 +0000

    added all kube code and files

--- resty/app.py
+++ resty/app.py
@@ -2,24 +2,71 @@ from flask import Flask, request
 from flask_restful import Resource, Api, abort, reqparse
 import pandas as pd
 import ast
+import yaml
+import time
+from kubernetes import client, config, watch
+
 
 app = Flask(__name__)
 api = Api(app)
 
+JOB_NAME = 'simple-job'
+kubernetes_config_file_path = 'kube-config'
+
 data = []i # Initialize empty list 
 
-@app.route('/connection', methods=['POST']) # '/connection' is our entry point for connection stuff
 
+def create_job_object():
+    # Configureate Pod template container
+    container = client.V1Container(
+        name='busybox',
+        image='busybox',
+        args=['sleep', '6'])
+    # Create and configurate a spec section
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels={'name': 'simple-job'}),
+        spec=client.V1PodSpec(restart_policy='OnFailure', containers=[container]))
+    # Create the specification of deployment
+    spec = client.V1JobSpec(template=template)
+    # Instantiate the job object
+    job = client.V1Job(
+        api_version='batch/v1',
+        kind='Job',
+        metadata=client.V1ObjectMeta(name=JOB_NAME),
+        spec=spec)
+
+    return job
+
+def create_job(api_instance, job):
+    api_response = api_instance.create_namespaced_job(
+        body=job,
+        namespace='default')
+    print("Job created. status='%s'" % str(api_response.status))
+
+
+
+def main():
+    config.load_kube_config(kubernetes_config_file_path)
+    batch_v1 = client.BatchV1Api()
+    job = create_job_object()
+
+    create_job(batch_v1, job)
+    time.sleep(30)
+
+
+@app.route('/connection', methods=['POST']) # '/connection' is our entry point for connection stuff
 def con(): # methods go here
 
     data.append(request.get_json())  # add args to data
-    return {'data': data} , 200 # return the data 
+    main()
+    return {'data': data} , 200 # return the data
+
 
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")  # run our Flask app
-
+    
 # tried to do something complex with classe and it didn't work so leave the code here
 
 #class Connection(Resource):
