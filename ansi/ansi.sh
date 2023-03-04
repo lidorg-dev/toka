e64c7ec2e89554d0523badb1cade534b3b9cff87
Ubuntu <ubuntu@ip-172-31-80-143.ec2.internal>
2023-03-04 12:28:42 +0000

    fixed ansible scirpt

--- ansi/ansi.sh
+++ ansi/ansi.sh
@@ -3,4 +3,12 @@ sudo apt update -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+cd ansible
+if ! command -v ansible-playbook &> /dev/null
+then
+    echo "ansible could not be found, the scirpt exit in ERROR"
+    exit
+fi
 
+
+ansible-playbook playbooks/web.yaml
